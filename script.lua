b2c49c42951d0e2f2fa628c6559430b78c8294d1
marknoodev <markdev0900@gmail.com>
2026-01-24 16:51:56 -0300

    Enhance ScreenGui with background and button functionalities
    Added background frame and extra folder to ScreenGui, along with new button functionalities for creating extra buttons and mod buttons. Updated existing functions to accommodate new features.

--- script.lua
+++ script.lua
@@ -14,12 +14,28 @@ local Animator = Humanoid:WaitForChild("Animator")
 
 -- Instances
 local ScreenGui = Instance.new("ScreenGui")
+local bg = Instance.new("Frame")
+local extraFolder = Instance.new("Folder")
 
 ScreenGui.Name = "ZM V1.2"
 ScreenGui.Enabled = false
 ScreenGui.Parent = Player:WaitForChild("PlayerGui")
 ScreenGui.ScreenInsets = Enum.ScreenInsets.DeviceSafeInsets
 ScreenGui.ResetOnSpawn = false
+
+bg.Visible = false
+bg.Name = "bg"
+bg.Parent = ScreenGui
+bg.AnchorPoint = Vector2.new(0.5, 0.5)
+bg.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+bg.BackgroundTransparency = .7
+bg.BorderColor3 = Color3.fromRGB(0)
+bg.BorderSizePixel = 0
+bg.Position = UDim2.new(0.5, 0, 0.5, 0)
+bg.Size = UDim2.new(1, 0, 1, 0)
+
+extraFolder.Name = "Extras"
+extraFolder.Parent = ScreenGui
 
 function createMenu(name, sizeX)
 	local Frame = Instance.new("Frame")
@@ -124,7 +140,180 @@ local canStartVK = false
 
 local vKEnabled = false
 
+local oldSelectedAnim = nil
+local selectedAnim = nil
+
 -- FUNCTIONS
+
+local function createExtraButton(name, parent) -- idk if i will work futurely on No Toggle version. it only works with toggle btw
+	local tb = Instance.new("TextButton")
+	tb.Parent = parent
+	tb.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+	tb.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	tb.BorderSizePixel = 0
+	tb.AutoButtonColor = false
+	tb.Font = Enum.Font.Sarpanch
+	tb.Text = tostring(name)
+	tb.TextColor3 = Color3.fromRGB(255, 255, 255)
+	tb.TextScaled = true
+	tb.TextWrapped = true
+	tb:SetAttribute("activated", false)
+
+	tb.MouseButton1Down:Connect(function()
+		if tb:GetAttribute("activated") then
+			tb:SetAttribute("activated", false) -- disabled
+			tb.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+			selectedAnim = nil
+		else
+			if selectedAnim == nil then
+				selectedAnim = tb
+			end
+
+			if selectedAnim ~= nil then
+				oldSelectedAnim = selectedAnim
+				oldSelectedAnim:SetAttribute("activated", false)
+				oldSelectedAnim.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+				selectedAnim = tb
+			end
+
+			selectedAnim:SetAttribute("activated", true) -- enabled
+			selectedAnim.BackgroundColor3 = Color3.fromRGB(0, 163, 166)
+		end
+	end)
+end
+
+local function createModButton(name, category, toggle, code, extraCode)
+	local btn = Instance.new("TextButton")
+
+	for _, cat in pairs(ScreenGui:GetDescendants()) do
+		if cat.Name == category then
+			btn.Parent = cat:FindFirstChild("holder")
+		end
+	end
+
+	btn.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+	btn.BorderSizePixel = 0
+	btn.Font = Enum.Font.Sarpanch
+	btn.Text = name
+	btn.TextColor3 = Color3.fromRGB(255, 255, 255)
+	btn.TextScaled = true
+	btn.AutoButtonColor = false
+	btn:SetAttribute("activated", false)
+
+	btn.MouseButton1Down:Connect(function()
+		if toggle then
+			if btn:GetAttribute("activated") then
+				btn:SetAttribute("activated", false) -- disabled
+				btn.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+				code(false)
+			else
+				btn:SetAttribute("activated", true) -- enabled
+				btn.BackgroundColor3 = Color3.fromRGB(0, 163, 166)
+				code(true)
+			end
+		else
+			if btn.BackgroundColor3 == Color3.fromRGB(127, 0, 0) then return end
+			btn.BackgroundColor3 = Color3.fromRGB(127, 0, 0)
+
+			code()
+		end
+	end)
+
+	if extraCode then
+		local ScrollingFrame = Instance.new("ScrollingFrame")
+		local TextLabel = Instance.new("TextLabel")
+		local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
+		local extraHolder = Instance.new("Frame")
+		local UIGridLayout = Instance.new("UIGridLayout")
+		local extraHider = Instance.new("TextButton")
+		
+		ScrollingFrame.Name = tostring(name)
+		ScrollingFrame.Visible = false
+		ScrollingFrame.Parent = extraFolder
+		ScrollingFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+		ScrollingFrame.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
+		ScrollingFrame.BorderSizePixel = 0
+		ScrollingFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
+		ScrollingFrame.Size = UDim2.new(0.17, 0, 0.7, 0)
+		ScrollingFrame.CanvasSize = UDim2.new(0, 0, 0, 0) -- IMPORTANTE: mudar para 0
+		ScrollingFrame.ScrollBarThickness = 6 -- Tornar a barra visível
+		ScrollingFrame.AutomaticCanvasSize = Enum.AutomaticSize.Y
+		ScrollingFrame.ScrollingEnabled = true
+		ScrollingFrame.ScrollBarImageColor3 = Color3.fromRGB(100, 100, 100) -- Opcional: cor da barra
+		
+		TextLabel.Parent = ScrollingFrame
+		TextLabel.BackgroundColor3 = Color3.fromRGB(38, 38, 38)
+		TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+		TextLabel.BorderSizePixel = 0
+		TextLabel.Size = UDim2.new(1, 0, 0.04, 0)
+		TextLabel.Font = Enum.Font.Sarpanch
+		TextLabel.Text = "Extra"
+		TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.TextScaled = true
+		TextLabel.TextWrapped = true
+
+		UITextSizeConstraint.Parent = TextLabel
+		UITextSizeConstraint.MaxTextSize = 33
+
+		extraHolder.Name = "extraHolder"
+		extraHolder.Parent = ScrollingFrame
+		extraHolder.BackgroundColor3 = Color3.fromRGB(34, 34, 34)
+		extraHolder.BackgroundTransparency = 1
+		extraHolder.BorderSizePixel = 0
+		extraHolder.Position = UDim2.new(0, 0, 0.04, 0)
+		extraHolder.Size = UDim2.new(1, 0, 0.96, 0)
+		extraHolder.ZIndex = 2
+
+		UIGridLayout.Parent = extraHolder
+		UIGridLayout.FillDirection = Enum.FillDirection.Vertical
+		UIGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+		UIGridLayout.CellPadding = UDim2.new(0, 0, 0, 0)
+		UIGridLayout.CellSize = UDim2.new(1, 0, 0.07, 0)
+
+		extraHider.Name = "extraHider"
+		extraHider.Parent = TextLabel
+		extraHider.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		extraHider.BackgroundTransparency = 1
+		extraHider.BorderSizePixel = 0
+		extraHider.Position = UDim2.new(0.8, 0, 0, 0)
+		extraHider.Size = UDim2.new(0.2, 0, 1, 0)
+		extraHider.Font = Enum.Font.Sarpanch
+		extraHider.Text = "-"
+		extraHider.TextColor3 = Color3.fromRGB(255, 255, 255)
+		extraHider.TextScaled = true
+		extraHider.TextWrapped = true
+
+		extraHider.MouseButton1Down:Connect(function()
+			ScrollingFrame.Visible = false
+			bg.Visible = false
+
+			-- need to add background working
+
+			for _, v in pairs(ScreenGui:GetChildren()) do
+				if v:IsA("Frame") then
+					if v.Name ~= "bg" then
+						v.Visible = true
+					end
+				end
+			end
+		end)
+
+		btn.MouseButton2Down:Connect(function()
+			ScrollingFrame.Visible = not ScrollingFrame.Visible
+			bg.Visible = true
+
+			for _, v in pairs(ScreenGui:GetChildren()) do
+				if v:IsA("Frame") then
+					if v.Name ~= "bg" then
+						v.Visible = not ScrollingFrame.Visible
+					end
+				end
+			end
+
+			extraCode()
+		end)
+	end
+end
 
 local function cleanupConnections()
 	for _, connection in ipairs(movesConnector) do
@@ -185,44 +374,6 @@ createMenu("Combat", .1)
 createMenu("Player", .3)
 createMenu("Visuals", .5)
 createMenu("Miscellaneous", .7)
-
-local function createModButton(name, category, toggle, code)
-	local btn = Instance.new("TextButton")
-
-	for _, cat in pairs(ScreenGui:GetDescendants()) do
-		if cat.Name == category then
-			btn.Parent = cat:FindFirstChild("holder")
-		end
-	end
-
-	btn.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
-	btn.BorderSizePixel = 0
-	btn.Font = Enum.Font.Sarpanch
-	btn.Text = name
-	btn.TextColor3 = Color3.fromRGB(255, 255, 255)
-	btn.TextScaled = true
-	btn.AutoButtonColor = false
-	btn:SetAttribute("activated", false)
-
-	btn.MouseButton1Down:Connect(function()
-		if toggle then
-			if btn:GetAttribute("activated") then
-				btn:SetAttribute("activated", false) -- disabled
-				btn.BackgroundColor3 = Color3.fromRGB(44, 44, 44)
-				code(false)
-			else
-				btn:SetAttribute("activated", true) -- enabled
-				btn.BackgroundColor3 = Color3.fromRGB(0, 163, 166)
-				code(true)
-			end
-		else
-			if btn.BackgroundColor3 == Color3.fromRGB(127, 0, 0) then return end
-			btn.BackgroundColor3 = Color3.fromRGB(127, 0, 0)
-
-			code()
-		end
-	end)
-end
 
 -- M1 Reset
 local m1Connection
@@ -297,8 +448,6 @@ createModButton("Void Kill", "Combat", true, function(isEnabled)
 end)
 
 -- Force AutoRotate
-
-
 
 local forceAutoRotateConnection
 
@@ -535,26 +684,122 @@ createModButton("KJ Flexworks Anim", "Miscellaneous", false, function()
 	end)
 end)
 
-local counterID = "rbxassetid://12351854556"
+-- Fake Anims
 
-local counterAnim = Instance.new("Animation")
-counterAnim.AnimationId = counterID
+local fakeAnimConnection
 
-local counterTrack = Animator:LoadAnimation(counterAnim)
+local Extras = ScreenGui:WaitForChild("Extras")
 
--- Fake Counter
-createModButton("Fake Counter", "Miscellaneous", false, function()
-	uis.InputBegan:Connect(function(i, p)
-		if p then return end
-		
-		if i.KeyCode == Enum.KeyCode.X then
-			if counterTrack.IsPlaying then
-				counterTrack:Stop()
+local AnimIDS = {
+	["Normal Punch"] = 10468665991,
+	["Consecutive Punches"] = 10466974800,
+	["Shove"] = 10471336737,
+	["Uppercut"] = 12510170988,
+	["Saitama Ult"] = 12447707844,
+	["Table Flip"] = 11365563255,
+	["Serious Punch"] = 12983333733,
+	["Omni Directional Punch"] = 13927612951,
+	["Mosquito"] = 140164642047188,
+
+	-- Garou
+	["Garou Ult"] = 12342141464,
+	["Flowing Water"] = 12272894215,
+	["Lethal Whirlwind Stream"] = 12296882427,
+	["Hunter's Grasp"] = 12307656616,
+	["Prey's Peril"] = 12351854556,
+	["Water Stream Cutting Fist"] = 12460977270,
+	["The Final Hunt"] = 12463072679,
+	["Rock Splitting Fist"] = 14057231976,
+	["Crushed Rock"] = 13630786846,
+
+	-- Genos
+	["Genos Ult"] = 12772543293,
+	["Machine Gun Blows"] = 12534735382,
+	["Thunder Kick"] = 14721837245,
+	["Speedblitz Dropkick"] = 12832505612,
+	["Flamewave Cannon"] = 13083332742,
+	["Incinerate"] = 13146710762,
+
+	-- Sonic
+	["Sonic Ult"] = 13499771836,
+	["Flash Strike"] = 13309500827,
+	["Whirlwind Kick"] = 13294790250,
+	["Explosive Shuriken"] = 13501296372,
+	["Carnage"] = 13723174078,
+}
+
+
+local currentTrack = nil
+
+local function playAnim()
+	local anim = Instance.new("Animation")
+	anim.AnimationId = "rbxassetid://" .. AnimIDS[selectedAnim.Text]
+	currentTrack = Animator:LoadAnimation(anim)
+	currentTrack.Priority = Enum.AnimationPriority.Action4
+
+	if currentTrack.IsPlaying then
+		currentTrack:Stop()
+	end
+
+	currentTrack:Play()
+end
+
+createModButton("Fake Anims", "Miscellaneous", true, function(isEnabled)
+	if isEnabled then
+		fakeAnimConnection = uis.InputBegan:Connect(function(i, p)
+			if p then return end
+
+			if i.KeyCode == Enum.KeyCode.X then
+				if selectedAnim ~= nil then
+					playAnim()
+				end
 			end
-			
-			counterTrack:Play()
+		end)
+	else
+		if fakeAnimConnection then
+			fakeAnimConnection:Disconnect()
+			fakeAnimConnection = nil
 		end
-	end)
+	end
+
+end, function() -- extra part
+	local location = Extras["Fake Anims"].extraHolder
+	createExtraButton("Normal Punch", location)
+	createExtraButton("Consecutive Punches", location)
+	createExtraButton("Shove", location)
+	createExtraButton("Uppercut", location)
+	createExtraButton("Saitama Ult", location)
+	createExtraButton("Table Flip", location)
+	createExtraButton("Serious Punch", location)
+	createExtraButton("Omni Directional Punch", location)
+	createExtraButton("Mosquito", location)
+
+	-- Garou
+	createExtraButton("Garou Ult", location)
+	createExtraButton("Flowing Water", location)
+	createExtraButton("Lethal Whirlwind Stream", location)
+	createExtraButton("Hunter's Grasp", location)
+	createExtraButton("Prey's Peril", location)
+	createExtraButton("Water Stream Cutting Fist", location)
+	createExtraButton("The Final Hunt", location)
+	createExtraButton("Rock Splitting Fist", location)
+	createExtraButton("Crushed Rock", location)
+
+	-- Genos
+	createExtraButton("Genos Ult", location)
+	createExtraButton("Machine Gun Blows", location)
+	createExtraButton("Thunder Kick", location)
+	createExtraButton("Speedblitz Dropkick", location)
+	createExtraButton("Flamewave Cannon", location)
+	createExtraButton("Incinerate", location)
+
+	-- Sonic
+	createExtraButton("Sonic Ult", location)
+	createExtraButton("Flash Strike", location)
+	createExtraButton("Whirlwind Kick", location)
+	createExtraButton("Explosive Shuriken", location)
+	createExtraButton("Carnage", location)
+
 end)
 
 Player.CharacterAdded:Connect(function(char)
@@ -564,8 +809,7 @@ Player.CharacterAdded:Connect(function(char)
 	Humanoid = char:WaitForChild("Humanoid")
 	HumanoidRootPart = char:WaitForChild("HumanoidRootPart")
 	Animator = Humanoid:WaitForChild("Animator")
-	counterTrack = Animator:LoadAnimation(counterAnim)
-	
+
 	-- Reloads the previous ' ON ' options
 	if forceAutoRotateConnection then -- it means it is on
 		forceAutoRotateCode()
