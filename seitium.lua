70b6288605b81c66a68010de6ff6e7de4bc81cb6
marknoodev <markdev0900@gmail.com>
2026-08-22 17:58:04 -0300

    Create seitium.lua

---
+++ seitium.lua
@@ -0,0 +1,22 @@
+local Ids = {
+	-- Blox Fruits
+	[2753915549] = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/marknoodev/modmenu/refs/heads/main/bloxfruits.lua"))()
+	end,
+	
+	-- The Strongest Battlegrounds
+	[10449761463] = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/marknoodev/modmenu/refs/heads/main/tsb.lua"))()
+	end,
+	
+	-- Flick
+	[136801880565837] = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/marknoodev/modmenu/refs/heads/main/flick.lua"))()
+	end,
+}
+
+local _script = Ids[game.PlaceId]
+
+if _script then
+	_script()
+end
